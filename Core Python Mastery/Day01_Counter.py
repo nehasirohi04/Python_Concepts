0606df559e1f630ba35cf8fd55dd64cbe9d665e1
Neha Sirohi <nehasirohi712@gmail.com>
2025-08-08 00:21:09 +0530

    👍 Day01 : Starting daily python practice.

---
+++ Core Python Mastery/Day01_Counter.py
@@ -0,0 +1,15 @@
+# 🧠 Objective:
+# Develop a Python script that reads a given text file and analyzes word frequency using the `collections.Counter` class.
+# This is a fundamental task in text processing and forms the basis for more advanced applications like search engines,
+# chatbots, and natural language processing (NLP) systems.
+
+text = """
+In the age of information, data is everywhere.
+But raw data alone is meaningless — it’s the patterns, the insights, the questions we ask of the data that give it value.
+Python, being a versatile and powerful language, enables us to extract this value efficiently.
+
+Can you count how often each word appears in this paragraph?
+Let’s find out — and remember: case, punctuation, and repetition all matter!
+"""
+# Lets start with normilization of input
+
